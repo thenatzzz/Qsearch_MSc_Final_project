d19921217e7969fa9aa305326df1471db2571ca8
Nattapat  Juthaprachakul <thenatzzz@gmail.com>
2018-08-22 21:47:30 +0100

    add rs graph

--- VISUALIZE_DIST/PLOT_DIST.py
+++ VISUALIZE_DIST/PLOT_DIST.py
@@ -17,29 +17,43 @@ def create_dist_chart(data,dataset,save_path):
     print("mean {} :: std {}".format(mean,std))
 
     mean_data = "Mean: {}".format(mean)
-    mean_line = plt.axvline(mean, color='red', linestyle='dashed', linewidth=2,label=mean_data)
+    # mean_line = plt.axvline(mean, color='red', linestyle='dashed', linewidth=2,label=mean_data)
+    mean_line = plt.axvline(mean, color='blue', linestyle='dashed', linewidth=2,label=mean_data)
+
 
     median = np.median(data)
     median_data = "Median: {}".format(median)
-    median_line = plt.axvline(median, color='green', linestyle='dashed', linewidth=2,label=median_data)
+    # median_line = plt.axvline(median, color='green', linestyle='dashed', linewidth=2,label=median_data)
+    median_line = plt.axvline(median, color='lightgreen', linestyle='dashed', linewidth=2,label=median_data)
+
 
     num_model = "Total number of model: {}".format(len(data))
     num_model_line = mpatches.Patch( label=num_model)
 
     pm = Symbol(u'±')
     std_data = "Mean {} 1 std.({})".format(pm,std)
-    std_line = plt.axvline(mean+std, color= 'violet', linestyle='dashed', linewidth=2, label= std_data)
-    std_line = plt.axvline(mean-std, color= 'violet', linestyle='dashed', linewidth=2, label= std_data)
+    # std_line = plt.axvline(mean+std, color= 'violet', linestyle='dashed', linewidth=2, label= std_data)
+    # std_line = plt.axvline(mean-std, color= 'violet', linestyle='dashed', linewidth=2, label= std_data)
+
+    max_data = "Highest accuracy: {} \nby Random Search".format(data.max())
+    max_line = plt.axvline(data.max(), color= 'red',linestyle='dashed', linewidth=2, label= max_data)
+
+    HIGH_ACC_RL = 0.9766
+    rl_data = "Highest accuracy: {} \nby Q-learning agent".format(HIGH_ACC_RL)
+    rl_line = plt.axvline(HIGH_ACC_RL, color= 'black',linestyle='dashed', linewidth=2, label= rl_data)
 
 
+    # plt.legend(handles= [mean_line,median_line,std_line,num_model_line])
+    # plt.legend(handles= [mean_line,median_line,max_line,num_model_line])
+    plt.legend(handles= [mean_line,median_line,max_line,rl_line,num_model_line])
 
-    plt.legend(handles= [mean_line,median_line,std_line,num_model_line])
-    # plt.legend(handles= [mean_line,median_line,num_model_line])
 
     sns.distplot(data,kde_kws={'clip': (0.0, 1.0)}).set(xlim=(0,1))
 
 
-    plt.title('Distribution of Validation Accuracy of Model found by Random Search on {}'.format(dataset))
+    # plt.title('Distribution of Validation Accuracy of Model found by Random Search on {}'.format(dataset))
+    plt.title('Distribution of Validation Accuracy of Model found by\n Q-learning agent and Random Search on {}'.format(dataset))
+
     plt.xlabel('Validation Accuracy')
     plt.ylabel('Number of Model')
     # plt.show()
