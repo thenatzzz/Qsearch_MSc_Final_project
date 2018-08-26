92336ea0c2e5bee3f8209aa1b175cbadf1c15d41
thenatzzz <thenatzzz@gmail.com>
2018-08-26 18:21:08 +0100

    add comment

--- VISUALIZE/FORMAT_TO_SNS_FORMAT.py
+++ VISUALIZE/FORMAT_TO_SNS_FORMAT.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,'/homes/nj2217/PROJECT/TMP4_CLEAN_CODE')
+sys.path.insert(0,'/homes/nj2217/FINAL_PROJECT/MAIN')
 
 from HELPER_FUNCTION import *
 import numpy as np
@@ -10,14 +10,21 @@ INDEX_LAYER_1 = 2
 INDEX_LAYER_2 = 3
 INDEX_LAYER_3 = 4
 INDEX_LAYER_4 = 5
+QTABLE_LENGTH = 6
 
 def begin_with_episode_word(first_element):
+    ###########################################################################
+    # FUNCTION DESCRIPTION: return true if the input word is 'episode'
+    ############################################################################
     if first_element[0:7] == 'episode':
         return True
     else:
         return False
-def check_begining_ending(single_data):
 
+def check_begining_ending(single_data):
+    ############################################################################
+    # FUNCTION DESCRIPTION: check the beginning of each episode in Q-table
+    ############################################################################
     if begin_with_episode_word(single_data[0]):
         return True
     elif len(single_data) == 1:
@@ -26,29 +33,36 @@ def check_begining_ending(single_data):
         return False
 
 def format_one_layer(index,value_in_one_layer, layer_action):
+    ############################################################################
+    # FUNCTION DESCRIPTION: format each layer of qtable per episode into this
+    #                       format: [ layer, action, qtable_val]
+    ############################################################################
     layer_num = index -1
     temp_list = []
     final_list = []
-    ''' [ layer, action, qtable_val] '''
     for i in range(len(value_in_one_layer)):
         action = layer_action[i]
-        # temp_list = [layer_num,action,value_in_one_layer[i] ]
         temp_list = [action,layer_num,value_in_one_layer[i] ]
 
         final_list.append(temp_list)
     return final_list
 
 def format_data_per_eps(single_qtable):
+    ############################################################################
+    # FUNCTION DESCRIPTION: main function to format Qtable for each episode
+    ############################################################################
     eps_name = single_qtable[INDEX_MODEL][0]
     format_array = []
     for index in range(INDEX_LAYER_1, INDEX_LAYER_4+1):
-        # format_array += format_one_layer(index,single_qtable[index], single_qtable[INDEX_ACTION])
         format_array += format_one_layer(index,single_qtable[index][:-1], single_qtable[INDEX_ACTION])
 
     return eps_name,format_array
 
 def is_complete_single_qtable(single_qtable):
-    if len(single_qtable) == 6:
+    ############################################################################
+    # FUNCTION DESCRIPTION: return true if single_qtable is complete
+    ############################################################################
+    if len(single_qtable) == QTABLE_LENGTH:
         return True
     else:
         return False
@@ -66,6 +80,10 @@ def save_single_eps_in_file(file_name, single_qtable,PATH):
     output_file = save_list_csv_rowbyrow(output_file,data,'w')
 
 def format_data(data, PATH):
+    ############################################################################
+    # FUNCTION DESCRIPTION: format each episode of Q-table in Seaborn data format
+    #                       into individual file and save into PATH
+    ############################################################################
     count = 0
     temp_array = []
     final_list = []
@@ -84,7 +102,6 @@ def format_data(data, PATH):
         temp_array.append(data[i])
         if is_complete_single_qtable(temp_array):
             eps_name,final_list = format_data_per_eps(temp_array)
-            # print(PATH)
             save_single_eps_in_file(eps_name,final_list,PATH)
             print("save ----> ", eps_name, " file successfully!")
             num_eps_saved += 1
