5b42da9efc4a38bdacc6212f34c7b491654910c8
Nattapat  Juthaprachakul <thenatzzz@gmail.com>
2018-08-26 14:27:45 +0100

    create hc

--- HILL_CLIMBING/HILL_CLIMBING_algo.py
+++ HILL_CLIMBING/HILL_CLIMBING_algo.py
@@ -28,19 +28,35 @@ def init_layer_action(final_array,index_hill_level):
         final_array[index+index_hill_level*MAX_ACTION][index_hill_level+1] = LAYER_ACTION[index]
     return final_array
 
+def used_best_layer(final_array,best_layer,index_hill_level):
+    for index in range(MAX_ACTION):
+        final_array[index+index_hill_level*MAX_ACTION][index_hill_level+1] = best_layer
+    return final_array
+
 def hill_climbing(DATASET):
     model_num = 0
     final_array = create_empty_array()
+    final_array = init_layer_action(final_array,index_hill_level)
 
     for index_hill_level in range(MAX_STATE):
-        final_array = init_layer_action(final_array,index_hill_level)
         for index_layer_in_hill in range(MAX_ACTION):
+            accuracy_array = []
             for index_model_array in range(MAX_INDEX_MODEL_ARRAY):
-                final_array[index_layer_in_hill+index_hill_level*MAX_ACTION][INDEX_MODEL] = 'model_'+str(model_num)
+                current_model_array = final_array[index_layer_in_hill+index_hill_level*MAX_ACTION]
+                current_model_array[INDEX_MODEL] = 'model_'+str(model_num)
 
                 if DATASET == 'cifar10':
-
+                    accuracy = random.randint(1,10)
+                    # accuracy = train_model_cifar10(current_model_array, DATASET)
                 elif DATASET == 'mnist':
+                    accuracy = random.randint(1,10)
+                    accuracy = train_model_mnist(current_model_array,DATASET)
+
+                accuracy_array.append(accuracy)
+
+            index_best_layer = accuracy_array.argmax()
+            best_layer = LAYER_ACTION[index_best_layer]
+            final_array = use_best_layer(final_array, best_layer,index_hill_level)
             model_num += 1
 
     print(final_array)
