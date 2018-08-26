fed00f8dbebb4ef71a607af0565b178ed34a06cd
Nattapat  Juthaprachakul <thenatzzz@gmail.com>
2018-08-26 15:32:43 +0100

    finish

--- HILL_CLIMBING/HILL_CLIMBING_algo.py
+++ HILL_CLIMBING/HILL_CLIMBING_algo.py
@@ -1,12 +1,12 @@
 import numpy as np
-
+import random
 import sys
 sys.path.insert(0,'/homes/nj2217/FINAL_PROJECT/MAIN')
 # sys.path.insert(0,'/d/PROJECT/FINAL_PROJECT/MAIN')
 
 from HELPER_FUNCTION import *
-from TRAIN_MODEL_CIFAR10 import *
-from TRAIN_MODEL_MNIST import *
+# from TRAIN_MODEL_CIFAR10 import *
+# from TRAIN_MODEL_MNIST import *
 
 MAX_ACTION = 16
 MAX_STATE = 4
@@ -15,6 +15,7 @@ LAYER_ACTION = ['c_1','c_2','c_3','c_4','c_5','c_6','c_7','c_8','c_9','c_10','c_
 
 MAX_INDEX_MODEL_ARRAY = 1 + MAX_STATE + 2
 INDEX_MODEL = 0
+INDEX_ACCURACY = -2
 
 def create_empty_array():
     final_array = []
@@ -28,17 +29,22 @@ def init_layer_action(final_array,index_hill_level):
         final_array[index+index_hill_level*MAX_ACTION][index_hill_level+1] = LAYER_ACTION[index]
     return final_array
 
-def used_best_layer(final_array,best_layer,index_hill_level):
-    for index in range(MAX_ACTION):
-        final_array[index+index_hill_level*MAX_ACTION][index_hill_level+1] = best_layer
+def use_best_layer(final_array,best_layer,index_hill_level):
+    start_index = (1+index_hill_level)*MAX_ACTION
+    # print(start_index)
+    final_index = MAX_STATE*MAX_ACTION
+    for index in range(start_index,final_index):
+        # print("index_hill_level->",index_hill_level)
+        final_array[index][index_hill_level+1] = best_layer
     return final_array
 
 def hill_climbing(DATASET):
     model_num = 0
     final_array = create_empty_array()
-    final_array = init_layer_action(final_array,index_hill_level)
 
     for index_hill_level in range(MAX_STATE):
+        final_array = init_layer_action(final_array,index_hill_level)
+        # print(final_array)
         for index_layer_in_hill in range(MAX_ACTION):
             accuracy_array = []
             for index_model_array in range(MAX_INDEX_MODEL_ARRAY):
@@ -46,20 +52,26 @@ def hill_climbing(DATASET):
                 current_model_array[INDEX_MODEL] = 'model_'+str(model_num)
 
                 if DATASET == 'cifar10':
-                    accuracy = random.randint(1,10)
+                    accuracy = random.uniform(0, 1)
                     # accuracy = train_model_cifar10(current_model_array, DATASET)
                 elif DATASET == 'mnist':
-                    accuracy = random.randint(1,10)
-                    accuracy = train_model_mnist(current_model_array,DATASET)
-
+                    accuracy = random.uniform(0, 1)
+                    # accuracy = train_model_mnist(current_model_array,DATASET)
+                current_model_array[INDEX_ACCURACY] =  accuracy
                 accuracy_array.append(accuracy)
 
-            index_best_layer = accuracy_array.argmax()
-            best_layer = LAYER_ACTION[index_best_layer]
-            final_array = use_best_layer(final_array, best_layer,index_hill_level)
+
             model_num += 1
 
+        max_accuracy = max(accuracy_array)
+        index_best_layer = accuracy_array.index(max_accuracy)
+        best_layer = LAYER_ACTION[index_best_layer]
+
+        if index_hill_level != MAX_STATE-1:
+            final_array = use_best_layer(final_array, best_layer,index_hill_level)
+
     print(final_array)
+
 def main():
     DATASET = 'cifar10'
     hill_climbing(DATASET)
