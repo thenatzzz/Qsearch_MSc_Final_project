96f6ae6e2b6c09fe19800965737716912e6b695d
thenatzzz <thenatzzz@gmail.com>
2018-08-26 18:36:05 +0100

    add comment to heatmap py

--- VISUALIZE/HEATMAP.py
+++ VISUALIZE/HEATMAP.py
@@ -14,6 +14,10 @@ import os, os.path
 import cv2
 
 def remove_space_csv(input_file, output_file):
+    ############################################################################
+    # FUNCTION DESCRIPTION: remove spaces between each episode of Qtable
+    ############################################################################
+
     with open(input_file, 'r') as inp:
         valid_rows = [row for row in csv.reader(inp) if any(field.strip() for field in row)]
 
@@ -21,6 +25,10 @@ def remove_space_csv(input_file, output_file):
         csv.writer(out).writerows(valid_rows)
 
 def add_zero(file_name):
+    ############################################################################
+    # FUNCTION DESCRIPTION: format file name by adding zero in order to get 4 digit format
+    ############################################################################
+
     name = file_name.strip('episode_')
     if len(name) == 3:
         name = '0'+name
@@ -31,13 +39,18 @@ def add_zero(file_name):
     return 'episode_'+name
 
 def get_episode_number(file_path):
+    ############################################################################
+    # FUNCTION DESCRIPTION: get episode number from file
+    ############################################################################
 
     start_index = file_path.index("episode_")
     episode_name = file_path[start_index:-1]
     return episode_name.strip('.csv')
 
 def create_heatmap(file_name,input_folder_loc, output_folder_loc):
-    # file_name = 'episode_3115.csv'
+    ############################################################################
+    # FUNCTION DESCRIPTION: create single heatmap png from 1 file csv
+    ############################################################################
     print("initial file_name: ",file_name)
     eps_file = pd.read_csv(file_name)
     eps_file = eps_file.pivot("Layer_type","Layer_number","Q-value")
@@ -45,9 +58,9 @@ def create_heatmap(file_name,input_folder_loc, output_folder_loc):
     cmap = sns.cm.rocket_r
     arr = eps_file.values
     vmin, vmax = arr.min(), arr.max()
-    # vmin = 0
-    # vmax = 1
-    # eps_file = (eps_file - eps_file.mean())/eps_file.std()
+    # vmin = 0   ### uncomment to set heatmap minimum value
+    # vmax = 1   ### uncomment to set heatmap maximum value
+    # eps_file = (eps_file - eps_file.mean())/eps_file.std()  ### uncommet to implement data normalization
     ax = sns.heatmap(eps_file, linewidths = 1, vmin=vmin, vmax=vmax,cmap = cmap, annot=True, fmt='.4g')
     ax.set_yticklabels(ax.get_yticklabels(), rotation = 0, fontsize = 10)
     ax2 = ax.get_figure()
@@ -55,29 +68,27 @@ def create_heatmap(file_name,input_folder_loc, output_folder_loc):
     title_name = get_episode_number(file_name)
     ax.set_title(title_name)
 
-    # SAVING_DIR = "/homes/nj2217/PROJECT/VISUALIZE/HEATMAP_PNG/"
     SAVING_DIR = output_folder_loc
-    # file_name = file_name.strip("/homes/nj2217/PROJECT/VISUALIZE/FORMATTED_EPS_FILE/")
     print("input_folder_loc : ", input_folder_loc)
     file_name = file_name.strip(input_folder_loc)
-    # print("file_name2 : ", file_name)
 
     file_name = file_name.strip(".csv")
-    # file_name = "e"+file_name
 
     print("complete file_name: ", file_name)
     file_name = add_zero(file_name)
-    print(file_name)
     png_file = SAVING_DIR + file_name+'.png'
-    # png_file = SAVING_DIR + file_name
     print("png_file: ", png_file)
     ax2.savefig(png_file,format='png')
     print("save: ", file_name ," heatmap in png successfully!")
     ax2.clf()
     # ax2.cla()
-    # plt.show()
+    # plt.show()    ### uncomment to show graph
 
 def execute(input_folder_loc, output_folder_loc):
+    ############################################################################
+    # FUNCTION DESCRIPTION: create bunch of heatmap from csv file in input folder
+    #                       and output heatmap png file to output folder
+    ############################################################################
     DIR = input_folder_loc
     number_of_file = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
     count = 0
@@ -91,12 +102,6 @@ def execute(input_folder_loc, output_folder_loc):
             print('\n')
             count += 1
 
-def create_vdo(path,vdo_name, framerate):
-   # os.system("cd "+path)
-#    os.system("cd /vol/gpudata/nj2217/HEATMAP/HEATMAP_PNG")
-    os.system("ffmpeg -framerate "+str(framerate)+" -i episode_%04d.png -c:v libx264 -r 30 -pix_fmt yuv420p "+path+"/"+vdo_name)
-    print("Create VDO of HEATMAP png_file named: ", vdo_name," successfully!!!")
-
 def main():
     input_file = "main.csv"
     output_file = "format_main.csv"
@@ -106,14 +111,8 @@ def main():
     OUTPUT_EP_FILE = "/vol/bitbucket/nj2217/FINAL_PROJECT/HEATMAP/EPISODE/"
     format_data(data, OUTPUT_EP_FILE)
 
-    # DIR = "/homes/nj2217/PROJECT/VISUALIZE/FORMATTED_EPS_FILE"
-    # SAVING_DIR = "/homes/nj2217/PROJECT/VISUALIZE/HEATMAP_PNG/"
-    # DIR = "/vol/gpudata/nj2217/HEATMAP/SNS_Q_TABLE"
-    # SAVING_DIR = "/vol/gpudata/nj2217/HEATMAP/HEATMAP_PNG/"
-
     HEATMAP_PNG_DIR = "/vol/bitbucket/nj2217/FINAL_PROJECT/HEATMAP/PNG/"
     HEATMAP_PNG_DIR = "/vol/bitbucket/nj2217/FINAL_PROJECT/HEATMAP/PNG2/"
-
 
     input_folder_loc = OUTPUT_EP_FILE
     output_folder_loc = HEATMAP_PNG_DIR
@@ -121,14 +120,12 @@ def main():
     execute(input_folder_loc, output_folder_loc)
     print("End execution!")
 
-    # VDO_NAME = "TEST_HEATMAP.mp4"
-    # FRAMERATE = 25
-    # VDO_PATH = "/vol/gpudata/nj2217/HEATMAP/HEATMAP_PNG"
-    # create_vdo(VDO_PATH,VDO_NAME,FRAMERATE)
-
 if "__main__" == __name__:
     main()
 
-
-# create vdo
+################# TO CREATE VIDEO OF PNG FILES #################################
+# REQUIREMENTs: 1. need ffmpeg installed
+#               2. execute command below to create vdo
+# note: can change framerate, file name, vdo output name
 # ffmpeg -framerate 25 -i episode_%04d.png -c:v libx264 -r 30 -pix_fmt yuv420p HEATMAP_cifar10.mp4
+################################################################################
